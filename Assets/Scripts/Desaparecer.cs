e20ac3c9e660905c7d4639e15cbc47c171cbe622
palomagc <pgcescorpio@msn.com>
2016-04-07 20:18:17 +0200

    Carga inicial
    Código desarrollado por Marta inicialmente

---
+++ Assets/Scripts/Desaparecer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Desaparecer : MonoBehaviour {
+
+    // En el entorno se determina el numero deveces que debe ser pulsado el obstaculo para eliminarlo
+    public int numVecesPulsacion; 
+
+    void OnMouseDown() { 
+        // IMPORTANTE -> todo objeto que use este script debe ser tagueado con Obstaculo
+        if(this.gameObject.tag == "Obstaculo"){
+            if (numVecesPulsacion != 1)
+                numVecesPulsacion--;
+            else
+                Destroy(gameObject, 0f); // Destruye el objeto pulsado
+        }
+    }
+
+}
---
+++ Assets/Scripts/EnfoqueMarcador.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Script que comprueba que el marcador esta siendo enfocado, si no, se envia un mensaje al usuario diciendo que lo enfoque..
+public class EnfoqueMarcador : MonoBehaviour {
+	private bool muestra_texto = false;
+	public string texto;
+	public GUISkin estilo;
+	void OnGUI(){
+		GUI.skin = estilo;
+		if (muestra_texto) {
+			GUI.skin.label.fontSize = (int) (Screen.width*5f)/50;
+			GUI.contentColor = Color.green;
+			GUI.skin.label.fontStyle = FontStyle.Bold;
+			GUI.skin.label.alignment = TextAnchor.LowerCenter;
+			GUI.Label(new Rect(0, Screen.height*0.10f, Screen.width, Screen.height*0.5f), texto); //45
+			GUI.skin.label.alignment = TextAnchor.UpperLeft;
+		}
+	}
+
+	public void marcadorEncontrado(){
+		muestra_texto = false;
+	}
+
+	public void marcadorPerdido(){
+		muestra_texto = true;
+	}
+}
