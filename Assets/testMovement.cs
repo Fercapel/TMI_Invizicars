aec39165e0497df6318aca06d97c0a29e049e1a0
Fernando <fercapel@ucm.es>
2016-04-15 19:18:52 +0200

    Creación parcial del trazado del circuito

---
+++ Assets/testMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testMovement : MonoBehaviour {
+
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Circuit"), "time", 15, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop, "orienttopath", true, "lookahead", .1f));
+	}
+}
